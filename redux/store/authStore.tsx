fd36eb8c4e32440d7e03c5d19136c8049523d57e
Aashish2221 <135332381+Aashish2221@users.noreply.github.com>
2025-08-21 16:28:08 +0530

    .

--- redux/store/authStore.tsx
+++ redux/store/authStore.tsx
@@ -3,19 +3,19 @@ import authReducer from "../features/authSlice";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { combineReducers } from "@reduxjs/toolkit";
-import { getDefaultMiddleware } from "@reduxjs/toolkit";
+// import { getDefaultMiddleware } from "@reduxjs/toolkit";
 const persistConfig = {
     key: "root",
     version: 1,
     storage
 }
 const reducer = combineReducers({ user: authReducer });
-const customMiddleWare = getDefaultMiddleware({
-    serializableCheck: false
-})
+// const customMiddleWare = getDefaultMiddleware({
+//     serializableCheck: false
+// })
 const persistedReducer = persistReducer(persistConfig, reducer);
 
 export const authStore = configureStore({
     reducer: persistedReducer,
-    middleware: customMiddleWare
+    // middleware: customMiddleWare
 })
