089f0bca369df014b91bc0932d557aa9c4bb10a2
Aashish2221 <135332381+Aashish2221@users.noreply.github.com>
2025-10-09 23:52:07 +0530

    change hider clour

--- componets/Header/page.tsx
+++ componets/Header/page.tsx
@@ -43,7 +43,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="bg-gray-600 text-white sticky top-0 z-50 shadow-lg">
+    <nav className="bg-black text-white sticky top-0 z-50 shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 sm:h-20">
           {/* Logo */}
